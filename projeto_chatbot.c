ecaa051973690a1a5b696f3362b6fd787d2324e5
Bruno Amorim <brunno00amorim@gmail.com>
2025-05-02 17:58:56 -0300

    [FEAT] Correção projeto_chatbot.c

--- projeto_chatbot.c
+++ projeto_chatbot.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <locale.h>
 
-int main() {
-	//declarações e configurações 
-    setlocale(LC_ALL, "Portuguese");
-	int opcao, subOpcao, qtd, i;
-    float totalPedido = 0.0;
-    const float taxaEntrega = 8.0;
-    int rodando = 1;
-    
-	//while que mantém o programa rodando: 
-    while (rodando) {
-    	system("cls"); // comando que limpa a tela
+int main (){
+	setlocale(LC_ALL, "Portuguese");
+	int rodando=1;
+	int opcao, subOpcao,qtd, i;
+	float taxaEntrega = 8.00;
+	float totalPedido = 0;
+	  
+	
+	while (rodando){
+	system("cls"); // comando que limpa a tela
         printf("\n=== BEM-VINDO À PIZZARIA DO CHATBOT ===\n"); //primeiro menu
         printf("Escolha uma opção:\n");
         printf("[1] Cardápio\n");
@@ -20,38 +19,40 @@ int main() {
         printf("[0] Sair\n");
         printf("Opção: ");
         scanf("%d", &opcao);
-		 // a partir da opção 
-        switch (opcao) {
-            case 1: // Parte [1] opção e subOpção
-            	system("cls"); // comando que limpa a tela
-                printf("\n--- CARDÁPIO ---\n");
-                printf("[1] Pizza de Calabresa - R$ 35.00\n");
-                printf("[2] Pizza de Frango c/ Catupiry - R$ 38.00\n");
-                printf("[3] Pizza Quatro Queijos - R$ 40.00\n");
-                printf("[0] voltar <-\n");
-                scanf("%d", &subOpcao);
-               		if(subOpcao != 0) {
-        				printf("Opção inválida! Aperte 0 para voltar ao menu principal.\n");
-        				scanf("%d", &subOpcao);
-        				system("pause");
-    				} 
-						 break;  //
+		
+		switch(opcao){
+				case 1:
 					
-            case 2:
-            	system("cls"); // comando que limpa a tela
-                printf("\n--- ATENDIMENTO ---\n");
-                printf("Horário de funcionamento: 18h às 23h\n");
-                printf("Endereço: Rua das Pizzas, 123\n");
-                printf("Telefone: (99) 99999-9999\n");
-                printf("[0] voltar <-\n");
-				scanf("%d", &subOpcao);
-               		if(subOpcao != 0) {
-        				printf("Opção inválida! Aperte 0 para voltar ao menu principal.\n");
-        				scanf("%d", &subOpcao);
-        				system("pause");
-    				} 
-						 break;
-            case 3: // parte[2] - Pedido 
+					system("cls");
+					printf("\n--- CARDÁPIO ---\n");
+	                printf("[1] Pizza de Calabresa - R$ 35.00\n");
+	                printf("[2] Pizza de Frango c/ Catupiry - R$ 38.00\n");
+	                printf("[3] Pizza Quatro Queijos - R$ 40.00\n");
+	                printf("[0] voltar <-\n");
+	                	do{
+	                	printf("Digite [0] para voltar <- \n");
+						scanf("%d",&subOpcao);	
+						}while(subOpcao!=0);
+	              
+					break;
+				case 2:
+					system("cls");
+					printf("\n--- ATENDIMENTO ---\n");
+                    printf("Horário de funcionamento: 18h às 23h\n");
+                    printf("Endereço: Rua das Pizzas, 123\n");
+                    printf("Telefone: (99) 99999-9999\n");
+                    printf("[0] Voltar <-\n");
+                    printf("Digite uma opção: ");
+                    scanf("%d", &subOpcao);
+                    	do{
+	                	printf("Digite [0] para voltar <- \n");
+						scanf("%d",&subOpcao);	
+						}while(subOpcao!=0);
+	              
+					break;
+		
+		
+				case 3: // parte[2] - Pedido 
                 printf("\n--- FAZER PEDIDO ---\n");
                 printf("Quantas pizzas você deseja pedir? ");
                 scanf("%d", &qtd);
